90588f9db9b317bdea35a39cc95d23bb89e28e95
CV <CV@CV-PC>
2020-06-19 23:58:37 +0530

    Added TakeScreenshot to LaunchingBrowser

--- Selenium/src/launchingBrowser/LaunchingBrowser.java
+++ Selenium/src/launchingBrowser/LaunchingBrowser.java
@@ -1,10 +1,16 @@
 package launchingBrowser;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import com.google.common.io.Files;
 
 public class LaunchingBrowser {
 	
@@ -17,11 +23,15 @@ public class LaunchingBrowser {
 
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.get("https://www.urbanladder.com/");
 		driver.findElement(By.xpath("//div[@id='login_dialog']/descendant::a[contains(text(),'Close')]")).click();
 		Actions action = new Actions(driver);
 		action.moveToElement(driver.findElement(By.xpath("//div[@id='topnav_wrapper']//span[contains(text(),'Sale')]"))).perform();
+		TakesScreenshot sc =(TakesScreenshot)driver;
+		File source= sc.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(source, new File("./Screenshots/urbanladder.png"));
+		
 		Thread.sleep(3000);
 		driver.close();
 
